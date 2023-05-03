f77db9f24b191d91d020e020b9db62eb5fd0874d
V0r1ex <vanutin.vitaly@yandex.ru>
2023-05-03 23:50:07 +0300

    second

--- js/script.js
+++ js/script.js
@@ -1,13 +1,21 @@
 const navbar = document.querySelector('.nav__mobile-list')
 const navBtn = document.querySelector('.nav__mobile_btn')
-//const scrollBtn = document.querySelector('.scroll-btn')
+const scrollBtn = document.querySelector('.scroll-btn')
 
-// скрытие/показ кнопки прокрутки
-// window.addEventListener('scroll', showScrollButton)
-// function showScrollButton() {
-//     if (window.scrollY > 500) scrollBtn.style.animation = 'appearance 1s forwards'   
-//     else if(getComputedStyle(scrollBtn).getPropertyValue('opacity') > 0)  scrollBtn.style.animation = 'disappearance 1s forwards'
-// }
+//скрытие/показ кнопки прокрутки
+window.addEventListener('scroll', showScrollButton)
+scrollBtn.addEventListener('click', scrollWindowToTop)
+
+//появление и исчезновение кнопки
+function showScrollButton() {
+    if (window.scrollY > 500) {
+        scrollBtn.style.animation = 'appearance 1s forwards' 
+        scrollBtn.style.pointerEvents = 'all'   
+    } else if (getComputedStyle(scrollBtn).getPropertyValue('opacity') == '1') {
+        scrollBtn.style.animation = 'disappearance 1s forwards'
+        scrollBtn.style.pointerEvents = 'none' 
+    }  
+}
 
 // скрытие навбара
 document.addEventListener('click', (event) => {
@@ -21,4 +29,16 @@ document.addEventListener('click', (event) => {
 navBtn.onclick = () => {
     navbar.style.display = 'flex'
     navbar.classList.toggle('open')
+}
+
+//плавный скролл до верха
+function scrollWindowToTop() {
+    let coffScroll = 10
+    let start = setInterval(() => {
+        if (window.scrollY > 0) {
+            coffScroll += 0.7
+            document.documentElement.scrollTop = window.scrollY - coffScroll
+        }
+        else clearInterval(start)
+    }, 1)
 }
